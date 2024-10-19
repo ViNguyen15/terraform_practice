1018999a05f4fda390d789df87d37b6e91c3a837
Fluffy <fluffywin@gmail.com>
2024-10-19 04:56:53 -0400

    first launch fully successful 1.0

--- practice.tf
+++ practice.tf
@@ -25,17 +25,19 @@ resource "linode_instance" "mochi_home" {
   count = 1  # This will create 3 instances
 
   label  = "Mochi_Home_${count.index + 1}"  # Use count.index to generate unique labels
-  image  = "linode/CentOS7"
+  image  = "linode/centos-stream9"
   type   = "g6-nanode-1"
   region = "us-southeast"
+  root_pass = var.root_password
 
   provisioner "remote-exec" {
+
 
     connection {
       type        = "ssh"
       user        = "root"
       password    = var.root_password
-      timeout     = "2m"
+      timeout     = "5m"
       host        = self.ip_address
       port        = 22
       
@@ -43,6 +45,7 @@ resource "linode_instance" "mochi_home" {
 
   
     inline = [
+      "sudo yum clean all",
       "sudo yum update -y",
       "sudo yum install -y git",
       "sudo yum install -y yum-utils device-mapper-persistent-data lvm2",
@@ -51,14 +54,23 @@ resource "linode_instance" "mochi_home" {
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
       "sudo yum install -y gnome-terminal",
-      "sudo usermod -aG docker $USER",
+      "sudo usermod -aG docker root",
       "sudo yum install -y epel-release",
       "sudo yum install -y docker-compose",
+      "docker pull vinguyen15/weightlifter-api:version1.0",
+      "docker run -d -p 8080:8080 vinguyen15/weightlifter-api:version1.0",
+      "echo 'Setup complete'",
     ]
   }
+}
+
+resource "time_sleep" "wait_for_instance" {
+  depends_on = [linode_instance.mochi_home]
+  create_duration = "60s"  # Wait for 60 seconds
 }
 
 
 output "linode_ip" {
   value = linode_instance.mochi_home[0].ipv4
 }
+
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "access_token"{
     description = "access api token for linode"
     type = string
-    default = ""
+    sensitive = true
 }
 
 variable "root_password"{
     description = "password to host"
     type = string
-    default = ""
+    sensitive = true
 }
